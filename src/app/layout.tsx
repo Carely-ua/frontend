0ac82b7c69ffa8c1a142cba04205d350f8a587eb
IlliaPruskii <illia.pruskii@gmail.com>
2023-07-13 17:53:18 +0300

    [CAR-5]: add basic styles system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer, Header } from '@/components'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import '@/scss/main.scss';
 
 const inter = Inter({ subsets: ['latin'] })
 
