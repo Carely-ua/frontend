9cff7a0c46d54e44f542f15b81726ef359a472c4
IlliaPruskii <illia.pruskii@gmail.com>
2023-07-14 12:08:43 +0300

    Setup repository for frontend. Add next js, typescript and eslint. (#1)
    * [CAR-5]: add basic navigation structure
    
    * [CAR-5]: add basic styles system
    
    * [CAR-5]: add basic material-ui
    
    * [CAR-5]: add basic eslint rules

---
+++ src/elements/button/Button.tsx
@@ -0,0 +1,14 @@
+import useButton from '@mui/base/useButton';
+import { memo } from 'react';
+
+const ButtonComponent = () => {
+  const { getRootProps } = useButton();
+
+  return (
+    <button type="button" {...getRootProps()}>
+      Click Me
+    </button>
+  );
+};
+
+export const Button = memo(ButtonComponent);
