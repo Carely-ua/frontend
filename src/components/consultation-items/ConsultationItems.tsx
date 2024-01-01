7cc5b404bbfdac4a6a7cad7a230b55c5f2e60a12
AleksaYO <113035091+AleksaYO@users.noreply.github.com>
2024-01-01 16:23:57 +0200

    fix: Doctor card link (#9)
    * fix: Doctor card link
    
    * fix: Doctor card link
    
    * fix: DocCard
    
    * fix event bubbling for add to bag button
    
    * fix eslint errors
    
    ---------
    
    Co-authored-by: IlliaPruskii <Illia.pruskii@gmail.com>

--- src/components/consultation-items/ConsultationItems.tsx
+++ src/components/consultation-items/ConsultationItems.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode, SyntheticEvent } from 'react';
+
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
-import Link from 'next/link';
 import { ServicesTypes } from '@/services';
 import { Typography } from '@/ui-kit';
 import { Rating } from '../rating';
@@ -9,6 +10,7 @@ import { PriceBlock } from '../price-block';
 import { AddToBagButton } from '../add-to-bag-button';
 import styles from './ConsultationItems.module.scss';
 import { ConsultationItemContainer } from './ConsultationItemContainer';
+const Link = dynamic(() => import('next/link'), { ssr: false });
 
 interface ConsultationItemProps extends ServicesTypes.Doctor {
   serviceId: string;
@@ -48,70 +50,73 @@ export const ConsultationItem: FC<ConsultationItemProps> = ({
     { title: 'Членство в асоціаціях', description: associations },
     { title: 'Наукові роботи та патенти', description: scientificWorks },
   ];
+
   return (
     <ConsultationItemContainer>
-      <div className={styles.consultationItem}>
-        <Link href={`/doctor/${id}/${serviceId}`} className={styles.doctorImage}>
-          <Image src={image} layout="fill" objectFit="cover" alt="doctor" />
-        </Link>
-        <div className={styles.mainInfo}>
-          <div className={styles.mobileRating}>
-            <Rating />
+      <Link suppressHydrationWarning href={`/doctor/${id}/${serviceId}`} className={styles.link}>
+        <div className={styles.consultationItem}>
+          <div className={styles.doctorImage}>
+            <Image src={image} layout="fill" objectFit="cover" alt="doctor" />
           </div>
-          <Link href={`/doctor/${id}/${serviceId}`} className={styles.doctorName}>
-            <Typography component="h3" gutterBottom="md">
-              {name}
-            </Typography>
-          </Link>
-          {!!tags && (
-            <Typography component="h4" gutterBottom="md">
-              {tags.join(', ')}
-            </Typography>
-          )}
-          {!!title && (
-            <div className={styles.extraInfoItem}>
-              <div className={styles.extraInfoItemIcon}>
-                <SVG.DoctorMan />
-              </div>
-              <Typography component="p" color="dark-grey">
-                {title}
+          <div className={styles.mainInfo}>
+            <div className={styles.mobileRating}>
+              <Rating />
+            </div>
+            <div className={styles.doctorName}>
+              <Typography component="h3" gutterBottom="md">
+                {name}
               </Typography>
             </div>
-          )}
-          <div className={styles.extraInfoItem}>
-            <div className={styles.extraInfoItemIcon}>
-              <SVG.Portfolio />
+            {!!tags && (
+              <Typography component="h4" gutterBottom="md">
+                {tags.join(', ')}
+              </Typography>
+            )}
+            {!!title && (
+              <div className={styles.extraInfoItem}>
+                <div className={styles.extraInfoItemIcon}>
+                  <SVG.DoctorMan />
+                </div>
+                <Typography component="p" color="dark-grey">
+                  {title}
+                </Typography>
+              </div>
+            )}
+            <div className={styles.extraInfoItem}>
+              <div className={styles.extraInfoItemIcon}>
+                <SVG.Portfolio />
+              </div>
+              <Typography component="p" color="dark-grey">
+                {experience} років досвіду
+              </Typography>
             </div>
-            <Typography component="p" color="dark-grey">
-              {experience} років досвіду
-            </Typography>
+          </div>
+          <div className={styles.priceInfo}>
+            <div className={styles.rating}>
+              <Rating rating={rating} reviewsCount={reviewsCount} />
+            </div>
+            <div className={styles.price}>
+              <PriceBlock firstPrice={price} secondPrice={discountPrice} />
+            </div>
+            <AddToBagButton serviceId={serviceId} doctorId={id} />
           </div>
         </div>
-        <div className={styles.priceInfo}>
-          <div className={styles.rating}>
-            <Rating rating={rating} reviewsCount={reviewsCount} />
-          </div>
-          <div className={styles.price}>
-            <PriceBlock firstPrice={price} secondPrice={discountPrice} />
-          </div>
-          <AddToBagButton serviceId={serviceId} doctorId={id} />
-        </div>
-      </div>
-      {children}
-      {arr.some(item => item.description) && (
-        <ul className={styles.docInfoList}>
-          {arr.map(({ title, description }, index) => {
-            return (
-              !!description && (
-                <li key={index}>
-                  <Typography component="h3">{title}</Typography>
-                  <Typography component="p">{description}</Typography>
-                </li>
-              )
-            );
-          })}
-        </ul>
-      )}
+        {children}
+        {arr.some(item => item.description) && (
+          <ul className={styles.docInfoList}>
+            {arr.map(({ title, description }, index) => {
+              return (
+                !!description && (
+                  <li key={index}>
+                    <Typography component="h3">{title}</Typography>
+                    <Typography component="p">{description}</Typography>
+                  </li>
+                )
+              );
+            })}
+          </ul>
+        )}
+      </Link>
     </ConsultationItemContainer>
   );
 };
--- src/services/cart/graphql/__generated__/GetOrders.ts
+++ src/services/cart/graphql/__generated__/GetOrders.ts
@@ -1,10 +1,132 @@
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 import * as Types from '../../../../utils/graphql/__generated__/types';
 
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-export type GetOrdersQueryVariables = Types.Exact<{ [key: string]: never; }>;
+export type GetOrdersQueryVariables = Types.Exact<{ [key: string]: never }>;
 
+export type GetOrdersQuery = {
+  __typename?: 'Query';
+  orders?: Array<{
+    __typename?: 'Order';
+    id: string;
+    number?: number | null;
+    createdAt?: any | null;
+    orderItems: Array<{
+      __typename?: 'OrderItem';
+      id: string;
+      reviewed?: boolean | null;
+      number?: number | null;
+      endDate?: any | null;
+      createdAt?: any | null;
+      status?: Types.OrderStatus | null;
+      doctor?: {
+        __typename?: 'Doctor';
+        id: string;
+        name: string;
+        tags?: Array<string | null> | null;
+        image: string;
+        price?: number | null;
+        discountPrice?: number | null;
+      } | null;
+      service?: {
+        __typename?: 'Service';
+        id: string;
+        serviceType: Types.ServiceType;
+        name: string;
+        price?: number | null;
+        discountPrice?: number | null;
+        clinic?: {
+          __typename?: 'Clinic';
+          id: string;
+          mainImage?: string | null;
+          clinicType: Types.ClinicType;
+          name: string;
+        } | null;
+      } | null;
+    }>;
+  } | null> | null;
+};
 
-export type GetOrdersQuery = { __typename?: 'Query', orders?: Array<{ __typename?: 'Order', id: string, number?: number | null, createdAt?: any | null, orderItems: Array<{ __typename?: 'OrderItem', id: string, reviewed?: boolean | null, number?: number | null, endDate?: any | null, createdAt?: any | null, status?: Types.OrderStatus | null, doctor?: { __typename?: 'Doctor', id: string, name: string, tags?: Array<string | null> | null, image: string, price?: number | null, discountPrice?: number | null } | null, service?: { __typename?: 'Service', id: string, serviceType: Types.ServiceType, name: string, price?: number | null, discountPrice?: number | null, clinic?: { __typename?: 'Clinic', id: string, mainImage?: string | null, clinicType: Types.ClinicType, name: string } | null } | null }> } | null> | null };
-
-
-export const GetOrdersDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetOrders"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"orders"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"number"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"orderItems"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"reviewed"}},{"kind":"Field","name":{"kind":"Name","value":"number"}},{"kind":"Field","name":{"kind":"Name","value":"endDate"}},{"kind":"Field","name":{"kind":"Name","value":"createdAt"}},{"kind":"Field","name":{"kind":"Name","value":"status"}},{"kind":"Field","name":{"kind":"Name","value":"doctor"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"tags"}},{"kind":"Field","name":{"kind":"Name","value":"image"}},{"kind":"Field","name":{"kind":"Name","value":"price"}},{"kind":"Field","name":{"kind":"Name","value":"discountPrice"}}]}},{"kind":"Field","name":{"kind":"Name","value":"service"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"serviceType"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"price"}},{"kind":"Field","name":{"kind":"Name","value":"discountPrice"}},{"kind":"Field","name":{"kind":"Name","value":"clinic"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"mainImage"}},{"kind":"Field","name":{"kind":"Name","value":"clinicType"}},{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]}}]}}]} as unknown as DocumentNode<GetOrdersQuery, GetOrdersQueryVariables>;
+export const GetOrdersDocument = {
+  kind: 'Document',
+  definitions: [
+    {
+      kind: 'OperationDefinition',
+      operation: 'query',
+      name: { kind: 'Name', value: 'GetOrders' },
+      selectionSet: {
+        kind: 'SelectionSet',
+        selections: [
+          {
+            kind: 'Field',
+            name: { kind: 'Name', value: 'orders' },
+            selectionSet: {
+              kind: 'SelectionSet',
+              selections: [
+                { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'number' } },
+                { kind: 'Field', name: { kind: 'Name', value: 'createdAt' } },
+                {
+                  kind: 'Field',
+                  name: { kind: 'Name', value: 'orderItems' },
+                  selectionSet: {
+                    kind: 'SelectionSet',
+                    selections: [
+                      { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'reviewed' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'number' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'endDate' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'createdAt' } },
+                      { kind: 'Field', name: { kind: 'Name', value: 'status' } },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'doctor' },
+                        selectionSet: {
+                          kind: 'SelectionSet',
+                          selections: [
+                            { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'tags' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'image' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'price' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'discountPrice' } },
+                          ],
+                        },
+                      },
+                      {
+                        kind: 'Field',
+                        name: { kind: 'Name', value: 'service' },
+                        selectionSet: {
+                          kind: 'SelectionSet',
+                          selections: [
+                            { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'serviceType' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'price' } },
+                            { kind: 'Field', name: { kind: 'Name', value: 'discountPrice' } },
+                            {
+                              kind: 'Field',
+                              name: { kind: 'Name', value: 'clinic' },
+                              selectionSet: {
+                                kind: 'SelectionSet',
+                                selections: [
+                                  { kind: 'Field', name: { kind: 'Name', value: 'id' } },
+                                  { kind: 'Field', name: { kind: 'Name', value: 'mainImage' } },
+                                  { kind: 'Field', name: { kind: 'Name', value: 'clinicType' } },
+                                  { kind: 'Field', name: { kind: 'Name', value: 'name' } },
+                                ],
+                              },
+                            },
+                          ],
+                        },
+                      },
+                    ],
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+} as unknown as DocumentNode<GetOrdersQuery, GetOrdersQueryVariables>;
